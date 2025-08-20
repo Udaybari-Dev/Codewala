4f631b7df7e5a29ddaafb832d361b0281aefb04b
Udaybari-Dev <udaybari17@gmail.com>
2025-08-20 16:46:07 +0530

    create-problem-issue-fix

--- backend/src/index.js
+++ backend/src/index.js
@@ -32,3 +32,76 @@ app.use("/api/v1/problems" , problemRoutes);
 app.listen(process.env.PORT, () => {
     console.log("server is running on port 8080 ");
 })
+
+
+
+
+
+
+
+
+// // This is your Prisma schema file,
+// // learn more about it in the docs: https://pris.ly/d/prisma-schema
+
+// // Looking for ways to speed up your queries, or scale easily with your serverless or edge functions?
+// // Try Prisma Accelerate: https://pris.ly/cli/accelerate-init
+
+// generator client {
+//   provider = "prisma-client-js"
+//   output   = "../src/generated/prisma"
+// }
+
+// datasource db {
+//   provider = "postgresql"
+//   url      = env("DATABASE_URL")
+// }
+
+
+// enum UserRole {
+//   USER
+//   ADMIN
+// }
+
+// enum Difficulty{
+//   EASY
+//   MEDIUM
+//   HARD
+// }
+ 
+// model User {
+//   id        String @id @default(uuid())
+//   name      String?
+//   email     String   @unique
+//   image   String?
+//   role  UserRole @default(USER)
+//   password  String
+//   createdAt DateTime @default(now())
+//   updatedAt DateTime @updatedAt
+
+//   problems Problem[]
+// }
+
+// model Problem {
+//   id String @id @default(uuid())
+//   title String
+//   description String
+//   difficulty Difficulty
+//   tags String[]  // ["tag1" , "tag2" , "tag3"]
+//   userId String
+//   examples Json
+//   constraints String
+//   hints String?
+//   editorial String?
+
+//   testcases Json
+//   codeSnippets Json 
+//   referenceSolution Json
+
+//   createdAt DateTime @default(now())
+//   updatedAt DateTime @updatedAt
+
+//   //relationships
+
+//   user User @relation(  fields: [userId], references: [id], onDelete: Cascade)
+  
+// }
--- backend/src/libs/db.js
+++ backend/src/libs/db.js
@@ -2,7 +2,9 @@ import {PrismaClient} from "../generated/prisma/index.js";
 
 const globalForPrisma = globalThis;
 
-export const db = globalForPrisma.db || new PrismaClient();
+export const db = globalForPrisma.db || new PrismaClient({
+    log: ['query', 'error', 'warn'], // helpful while debugging
+  });
 
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = db;
  
