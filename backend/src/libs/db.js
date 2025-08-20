29a34552eb3a15e90c3782f1008224b51a094834
Udaybari-Dev <udaybari17@gmail.com>
2025-08-20 17:23:52 +0530

    fix-backend-issue

--- backend/src/libs/db.js
+++ backend/src/libs/db.js
@@ -2,9 +2,7 @@ import {PrismaClient} from "../generated/prisma/index.js";
 
 const globalForPrisma = globalThis;
 
-export const db = globalForPrisma.db || new PrismaClient({
-    log: ['query', 'error', 'warn'], // helpful while debugging
-  });
+export const db = globalForPrisma.db || new PrismaClient();
 
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = db;
  
