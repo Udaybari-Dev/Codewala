39d3092091a3b9df843cc7fd49020164f8240b47
Udaybari-Dev <udaybari17@gmail.com>
2025-09-12 12:07:33 +0530

    fixing some backend issue

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,8 +3,6 @@ import dotenv from "dotenv";
 import cookieparser from "cookie-parser";
 
 
-
-
 import authRoutes from "./routes/auth.routes.js";
 import problemRoutes from "./routes/problem.routes.js";
 
@@ -37,71 +35,3 @@ app.listen(process.env.PORT, () => {
 
 
 
-
-
-
-// // This is your Prisma schema file,
-// // learn more about it in the docs: https://pris.ly/d/prisma-schema
-
-// // Looking for ways to speed up your queries, or scale easily with your serverless or edge functions?
-// // Try Prisma Accelerate: https://pris.ly/cli/accelerate-init
-
-// generator client {
-//   provider = "prisma-client-js"
-//   output   = "../src/generated/prisma"
-// }
-
-// datasource db {
-//   provider = "postgresql"
-//   url      = env("DATABASE_URL")
-// }
-
-
-// enum UserRole {
-//   USER
-//   ADMIN
-// }
-
-// enum Difficulty{
-//   EASY
-//   MEDIUM
-//   HARD
-// }
- 
-// model User {
-//   id        String @id @default(uuid())
-//   name      String?
-//   email     String   @unique
-//   image   String?
-//   role  UserRole @default(USER)
-//   password  String
-//   createdAt DateTime @default(now())
-//   updatedAt DateTime @updatedAt
-
-//   problems Problem[]
-// }
-
-// model Problem {
-//   id String @id @default(uuid())
-//   title String
-//   description String
-//   difficulty Difficulty
-//   tags String[]  // ["tag1" , "tag2" , "tag3"]
-//   userId String
-//   examples Json
-//   constraints String
-//   hints String?
-//   editorial String?
-
-//   testcases Json
-//   codeSnippets Json 
-//   referenceSolution Json
-
-//   createdAt DateTime @default(now())
-//   updatedAt DateTime @updatedAt
-
-//   //relationships
-
-//   user User @relation(  fields: [userId], references: [id], onDelete: Cascade)
-  
-// }
