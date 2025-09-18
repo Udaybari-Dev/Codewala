1e0b3c5ff55e0b1749f3e9ab1a5f6076555d8b51
Udaybari-Dev <udaybari17@gmail.com>
2025-09-19 00:59:11 +0530

    execute-code-route

--- backend/src/libs/judge0.libs.js
+++ backend/src/libs/judge0.libs.js
@@ -63,6 +63,19 @@ export const submitBatch = async (submissions) => {
 
 
 
+export function getLanguageName(languageId){
+    const LANGUAGE_NAMES = {
+        74: "TypeScript",
+        63: "JavaScript",
+        71: "Python",
+        62: "Java",
+    }
+
+    return LANGUAGE_NAMES[languageId] || "Unknown"
+}
+
+
+
 
 
 
